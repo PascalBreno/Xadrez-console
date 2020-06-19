b4686123d4ac8751c78875ef34812f77704dc670
Pascal <brenodml@live.com>
2020-06-19 00:25:29 -0400

    Tratando erros com exceção personalizada

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -8,10 +8,17 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            
-            Tela.imprimirTabuleiro(tab);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 9));
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.Write(e.Message);
+            }
             
         }
     }    
--- Xadrez-console/tabuleiro/Tabuleiro.cs
+++ Xadrez-console/tabuleiro/Tabuleiro.cs
@@ -26,6 +26,10 @@
 
         public void colocarPeca (Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
